b5dfdb4e9ba442a4ca6d2a120cf1167821bc2a82
FrazShabbir <fraz.shabbir54@gmail.com>
2020-12-19 22:51:43 +0500

    exe file

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys                    #FORM ENABLEING KEY PRESSES
 from time import sleep
-#GmailAccount creator Script          Half
-#driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--incognito")
-# driver = webdriver.Chrome(chrome_options=chrome_options) 
-# 
 
 def gsearch():
     driver = webdriver.Chrome("/usr/local/share/chromedriver")
@@ -22,19 +15,3 @@ def gsearch():
 
 for x in range(1100):  
     gsearch() 
-
-
-
-
-
-# lname=driver.find_element_by_xpath('//*[@id="lastName"]')
-# lname.send_keys("shabbir")
-# sleep(3)
-# pass1=driver.find_element_by_xpath('//*[@id="passwd"]/div[1]/div/div[1]/input')
-# #passwordforuser=fname+lname
-# pass1.send_keys("passwordforuser")
-# pass2=driver.find_element_by_xpath('//*[@id="confirm-passwd"]/div[1]/div/div[1]/input')
-# pass2.send_keys("passwordforuser")
-
-# next1=driver.find_element_by_xpath('//*[@id="accountDetailsNext"]/span/span')
-# next1.click()
