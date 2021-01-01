f4e86bb1f43572db2cc2791a48cd4ec56151274f
FrazShabbir <fraz.shabbir54@gmail.com>
2021-01-01 23:58:29 +0500

    scraper

--- test.py
+++ test.py
@@ -7,9 +7,48 @@ import requests
 def gsearch():
     driver = webdriver.Chrome("/usr/local/share/chromedriver")
     
-
     what = input("Enter Job title:")
     where = input("Enter destination:")
+
+    link="https://pk.indeed.com/jobs?q="+what+"&l="+where
+    driver.get(link)
+    next1=driver.find_element_by_css_selector("h2.title")
+    next1.click()
+
+    source = requests.get(link).text
+    soup = BeautifulSoup(source,'lxml')
+    ass = soup.find('li').text
+    # for tag in soup.find_all("div",{"id":"vjs-desc"}):
+    #     print("{0}: {1}".format(tag.name, tag.text))
+    headline = ass
+    print(headline)
+    sleep(4000)
+    
+    
+gsearch()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # fname=driver.find_element_by_xpath('//*[@id="text-input-what"]')
     # fname.send_keys(what)
 
@@ -18,11 +57,3 @@ def gsearch():
 
     # next1=driver.find_element_by_xpath('//*[@id="whatWhereFormId"]/div[3]/button')
     # next1.click()
-    link="https://pk.indeed.com/jobs?q="+what+"&l="+where
-    driver.get(link)
-    source = requests.get(link).text
-    soup = BeautifulSoup(source,'lxml')
-    
-    
-    
-gsearch()
